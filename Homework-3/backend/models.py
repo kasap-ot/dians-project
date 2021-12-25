b818bd7bf72262c0639d37a17b9b568144950c5d
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2021-12-25 23:37:06 +0100

    Add db (sqlalchemy) models

---
+++ Homework-3/backend/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablenae__ = "restaurants"
+    id_ = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    rating = Column(Float)
+    lat = Column(Float, nullable=False)
+    lon = Column(Float, nullable=False)

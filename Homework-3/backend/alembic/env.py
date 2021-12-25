6c850c698519f1a04f4358456cca978ee91ee2b7
kasap-ot <63372251+kasap-ot@users.noreply.github.com>
2021-12-26 00:04:24 +0100

    Define db models location

--- Homework-3/backend/alembic/env.py
+++ Homework-3/backend/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import os, sys
+import os, sys, models
 from dotenv import load_dotenv
 
 
@@ -26,7 +26,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
